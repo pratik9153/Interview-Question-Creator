a9eee5264d87b0164b52513652b41f52abe93393
pratik9153 <pratikchoudhary316@gmail.com>
2024-11-19 18:02:57 +0530

    project

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup , find_packages
+
+setup(
+    name="Interview questions creator",
+    version="0.0.1",
+    author="Pratik",
+    author_email="pratikchoudhary316@gmail.com",
+    description="A interview question creator",  
+    packages=find_packages(),
+    install_requires=[],
+)
